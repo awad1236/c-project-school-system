6e5eedd458b2aab9bf457946a391363951fb3d3f
Mohamed_Awad <152029138+awad1236@users.noreply.github.com>
2024-08-26 19:04:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -4,4 +4,27 @@ typedef struct {
     double degree;
 
 } student;
+typedef struct {
+    char Teacher_Name[200];
+    char Teacher_Pass[200];
+
+} Teacher;
+typedef struct {
+    char Admin_Name[200];
+    char Admin_Pass[200];
+
+} Admin;
 int search(student a[2000],int x);
+void AddTeacher(void);
+void RemoveTeacher(void);
+void EditStudentDegree(void);
+void student_Search(void);
+void Dispaly(void);
+void DisplyAll(void);
+void Error(void) ;
+void WrongPass(void);
+void AddStudent(void);
+void Remove(void);
+void EditStudentInfo(void);
+int checkPassAdmin(void);
+int checkPassTeacher(void) ;
